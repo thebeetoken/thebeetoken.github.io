1ca852da313c592760b26ee4317d1abcf73f7dda
Kevin Van <kevinvan@Kevins-MacBook-Pro.local>
2017-09-10 11:45:23 -0700

    fix(scroll): can now scroll if text is off screen

--- bee-javascript.js
+++ bee-javascript.js
@@ -4,18 +4,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var beeLogoMain = document.getElementById('bn-logo-main');
   var mainCTA = document.getElementById('bn-main-cta');
 
+  // timeout for fade-in body
   setTimeout(function() {
     bodyEl.style.opacity = 1;
   });
 
+  // timeout for big logo fade out
   setTimeout(function() {
     beeLogo.style.opacity = 0;
   }, 1000);
 
+  //timeout for main logo fade in
   setTimeout(function() {
     beeLogoMain.style.opacity = 1;
   }, 2000);
 
+  //timeout for main body fade in
   setTimeout(function() {
     mainCTA.style.opacity = 1;
     mainCTA.style.pointerEvents = 'auto';
