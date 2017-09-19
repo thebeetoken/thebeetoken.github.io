04999db014ef2c72683a8f593b4cfa8cb26257d5
Michael Pak <pak.junho.michael@gmail.com>
2017-09-19 01:20:52 -0700

    Add incomplete hamburger menu

---
+++ frontend/components/hamburgerMenu/hamburgerMenu.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+require ('./hamburgerMenu.scss');
+
+class HamburgerMenu extends Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <Menu>
+        <a href="#about" className="bee-header-link">about</a>
+        <a href="#team" className="bee-header-link">team</a>
+        <a href="#advisors" className="bee-header-link">advisors</a>
+        <button href="#" className="bee-header-whitepaper-button">whitepaper</button>
+      </Menu>
+    );
+  }
+}
+
+export default HamburgerMenu;
+
+
--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import HamburgerMenu from '../hamburgerMenu/hamburgerMenu';
 require ("./header.scss");
 
 const Header = () => (
@@ -6,19 +7,20 @@ const Header = () => (
     <div className="bee-header-logo-container">
       <img className="bee-header-logo" src="http://res.cloudinary.com/duettwoxj/image/upload/v1505623250/logo-horizontal_zizcll.svg" />
     </div>
-    <div className="bee-header-links-container">
-      <a href="#about" className="bee-header-link">about</a>
-      <a href="#team" className="bee-header-link">team</a>
-      <a href="#advisors" className="bee-header-link">advisors</a>
-      <button href="#" className="bee-header-whitepaper-button">
-        whitepaper
-      </button>
-    </div>
+    <HamburgerMenu/>
   </nav>
 );
 
 export default Header;
 
 
+    //<div className="bee-header-links-container">
+      //<a href="#about" className="bee-header-link">about</a>
+      //<a href="#team" className="bee-header-link">team</a>
+      //<a href="#advisors" className="bee-header-link">advisors</a>
+      //<button href="#" className="bee-header-whitepaper-button">
+        //whitepaper
+      //</button>
+    //</div>
 
 
