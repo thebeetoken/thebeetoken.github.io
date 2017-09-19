5a0a5921d8dd24f4150bd9671736dc278dfa424f
Michael Pak <pak.junho.michael@gmail.com>
2017-09-18 21:23:01 -0700

    Fix Formspree email

--- frontend/components/email-form/email-form.jsx
+++ frontend/components/email-form/email-form.jsx
@@ -1,34 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 require('./email-form.scss');
 
-const EmailForm = () => {
-  let emailAddress;
+class EmailForm extends Component {
+  constructor(){
+    super();
+    this.state = { 
+      emailAddress: "",
+      submitStatus: true  
+    }
+  }
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  handleChange(e){
+    this.setState({ emailAddress: e.target.value }, () => this.emailChecker())
+  }
 
-    $.ajax({
-      method: "POST",
-      url: "/api/email",
-      data: emailAddress,
-      success: () => alert("Success!"),
-      error: () => alert("Failure!")
-    })
-  };
+  emailChecker(){
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const { emailAddress } = this.state;
 
-  return (
-    <div className="bee-email-form-container">
-      <form className="bee-main-cta-form" onSubmit={() => this.handleSubmit}>
-        <input 
-               className="bee-main-cta-email"
-               type="text"
-               ref={email_address => emailAddress = email_address}
-               placeholder="Email Address"/>
-        <button className="bee-main-cta-submit" type="submit" value="Submit">Submit</button>
-      </form>
-    </div>
-  )
+    if (emailRegex.test(emailAddress)){
+      this.setState({ submitStatus: false });
+      $(".bee-main-cta-cc").value = emailAddress;
+    }
+  }
+
+  handleSubmit(){
+    alert("Hello World");
+  }
+
+  render(){
+    return (
+      <div className="bee-email-form-container">
+        <form className="bee-main-cta-form" method="POST" action="http://formspree.io/thebeetoken@gmail.com">
+          <input className="bee-main-cta-email" id="bee-main-cta-email" type="email" name="email" placeholder="Your Email" autocomplete="off" onChange={(e) => this.handleChange(e)}/>
+          <input className="bee-main-cta-cc" id="bee-main-cta-cc" type="hidden" name="_cc" value="" />
+          <input className="bee-main-cta-hidden" type="hidden" name="_subject" value="Stay tuned for The Bee Token updates!" />
+          <input className="bee-main-cta-hidden" type="hidden" name="_next" value="https://thebeetoken.github.io" />
+          <input className="bee-main-cta-hidden" type="hidden" name="_format" value="plain" />
+          <button className="bee-main-cta-submit" id="bee-main-cta-submit" type="submit" disabled={this.state.submitStatus}>Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default EmailForm;
 
+
+        //<form className="bee-main-cta-form" onSubmit={() => this.handleSubmit()}>
+          //<input 
+                 //className="bee-main-cta-email"
+                 //type="text"
+                 //onChange={(e) => this.handleChange(e)}
+                 //placeholder="Email Address"/>
+          //<button 
+                 //className="bee-main-cta-submit" 
+                 //disabled={this.state.submitStatus}
+                 //type="submit" 
+                 //value="Submit">Submit
+          //</button>
+        //</form>
