e288038b18346739d7eff0b223d3f54f8edb3506
Kevin Van <kevinvan@Kevins-MacBook-Pro.local>
2017-09-20 00:20:09 -0700

    fix(cta): add the bee token

--- frontend/components/heroCta/heroCta.jsx
+++ frontend/components/heroCta/heroCta.jsx
@@ -8,7 +8,11 @@ const HeroCta = () => (
         <img className="bee-hero-cta-logo" src="http://res.cloudinary.com/duettwoxj/image/upload/v1505626872/logo-vertical_d1iixl.svg" />
       </div>
       <div className="bee-hero-cta-content">
-        <h1>Reinventing Housing Exchange</h1>
+        <h1>
+          The Bee Token:
+          <br />
+          Reinventing Housing Exchange
+        </h1>
         <p>Putting housing back into the hands of the consumer through the power of Ethereum blockchain.</p>
       </div>
     </div>
