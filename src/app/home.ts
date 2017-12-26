4c9fb3b52cc055d9a768d0956438f412e6ae4ddb
Jay Hwang <jayhwang0121@gmail.com>
2017-12-26 12:57:05 -0800

    cherry-pick merge: Create token distribution table

--- src/app/home.ts
+++ src/app/home.ts
@@ -4,8 +4,6 @@ import { Ng2PageScrollModule } from 'ng2-page-scroll';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { SubscribeComponent } from './subscribe/subscribe.component';
 import { HeroComponent } from './hero/hero.component';
 import { EmailFormComponent } from './email-form/email-form.component';
@@ -16,7 +14,7 @@ import { AdvisorsComponent } from './advisors/advisors.component';
 import { ValuepropsComponent } from './valueprops/valueprops.component';
 import { MediaComponent } from './media/media.component';
 import { EmailBarComponent } from './email-bar/email-bar.component';
-// import { TelegramBarComponent } from './telegram-bar/telegram-bar.component';
+
 import { RedirectComponent } from './redirect/redirect.component';
 import { WhitepaperGuardService } from './whitepaper-guard.service';
 
@@ -24,12 +22,9 @@ const appRoutes: Routes = [
   { path: 'whitepaper', component: RedirectComponent, canActivate: [WhitepaperGuardService] }
 ];
 
-
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     SubscribeComponent,
     HeroComponent,
     EmailFormComponent,
@@ -40,7 +35,7 @@ const appRoutes: Routes = [
     ValuepropsComponent,
     MediaComponent,
     EmailBarComponent,
-    RedirectComponent
+    RedirectComponent,
   ],
   imports: [
     BrowserModule,
@@ -52,4 +47,4 @@ const appRoutes: Routes = [
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class HomeModule { }
---
+++ src/app/token-distribution/token-distribution.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bee-token-distribution',
+  templateUrl: './token-distribution.component.html',
+  styleUrls: ['./token-distribution.component.scss']
+})
+export class TokenDistributionComponent {
+
+}
