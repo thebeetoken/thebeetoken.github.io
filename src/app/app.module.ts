89ef96e1ca7a317c5afda74b9372aeb4ff407874
Jay Hwang <jayhwang0121@gmail.com>
2017-12-29 12:38:45 -0800

    Create partners Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TokenDistributionComponent } from './token-distribution/token-distribut
 import { TeamComponent } from './team/team.component';
 import { AdvisorsComponent } from './advisors/advisors.component';
 import { ValuepropsComponent } from './valueprops/valueprops.component';
+import { PartnersComponent } from './partners/partners.component';
 import { MediaComponent } from './media/media.component';
 import { EmailBarComponent } from './email-bar/email-bar.component';
 // import { TelegramBarComponent } from './telegram-bar/telegram-bar.component';
@@ -38,6 +39,7 @@ const appRoutes: Routes = [
     TeamComponent,
     AdvisorsComponent,
     ValuepropsComponent,
+    PartnersComponent,
     MediaComponent,
     EmailBarComponent,
     RedirectComponent
